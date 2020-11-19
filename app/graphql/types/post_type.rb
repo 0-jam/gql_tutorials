32a1cdc048355f52a9903870541607b596ddd1f6
0-jam <28038981+0-jam@users.noreply.github.com>
2020-11-19 18:18:09 +0900

    Added some objects

---
+++ app/graphql/types/post_type.rb
@@ -0,0 +1,10 @@
+module Types
+  class PostType < Types::BaseObject
+    description 'A blog post'
+
+    field :id, ID, null: false
+    field :title, String, null: true
+    field :rating, Integer, null: true
+    field :comments, [Types::CommentType], null: true
+  end
+end
