f67722764910b3287768cb5515b3879cce14fb76
0-jam <28038981+0-jam@users.noreply.github.com>
2020-11-19 18:43:18 +0900

    Seeds for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create!(title: 'post1', rating: 4)
